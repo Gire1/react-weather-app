a1f081bbf3a43a0c8f77d32ae24fff721d4c48d3
Gire1 <168452060+Gire1@users.noreply.github.com>
2024-07-18 16:04:02 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    Weather App
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by{""}
+      <a href="#" target="_blank">
+         Grace Chifamba</a> {""}and is{""}
+    < a href="#" target="_blank">open-sourced on GitHub </a>
+    </footer>
+
     </div>
   );
 }
