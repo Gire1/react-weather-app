39a9c80868a94d10143026aa092348e1652a1ff7
Gire1 <168452060+Gire1@users.noreply.github.com>
2024-07-18 22:57:40 +0200

    Added HTML in the Weather.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-    <h1>Weather App</h1>
+    
+    < Weather />
     <footer>
       This project was coded by{""}
       <a href="https://earnest-haupia-0d9b3d.netlify.app/" target="_blank">
