15e20af30407f1fd3674b726cd59f235b4f734f3
Gire1 <168452060+Gire1@users.noreply.github.com>
2024-07-22 11:05:59 +0200

    Updated the index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-
-
+import React from "react";
+import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 
 
+import "./App.css";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <div className="App">
+      
+      <formatDate />
+    </div>
+  </React.StrictMode>
+);
 
